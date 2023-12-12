72f1de602c8a07fa5b3b647a73cb8cdc852e264c
Boris Kuzmic <boris.kuzmic@bloomreach.com>
2023-12-12 21:56:39 +0100

    Uploading bank statements to Dropbox and forwarding the email to destination

--- src/index.js
+++ src/index.js
@@ -1,15 +1,54 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import PostalMime from 'postal-mime';
+
+async function streamToArrayBuffer(stream, streamSize) {
+	let result = new Uint8Array(streamSize);
+	let bytesRead = 0;
+	const reader = stream.getReader();
+	while (true) {
+	  const { done, value } = await reader.read();
+	  if (done) {
+		break;
+	  }
+	  result.set(value, bytesRead);
+	  bytesRead += value.length;
+	}
+	return result;
+}
 
 export default {
-	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
+	async email(message, env, ctx) {
+		const allowList = env.ALLOWED_FROM_ADDRESSES;
+		if (allowList.indexOf(message.from) == -1) {
+			message.setReject("Address not allowed");
+		}
+		const rawEmail = await streamToArrayBuffer(message.raw, message.rawSize);
+		const parser = new PostalMime();
+		const parsedEmail = await parser.parse(rawEmail);
+		console.log("Mail subject: ", parsedEmail.subject);
+		if (parsedEmail.attachments.length == 0) {
+			console.log("No attachments - skipping");
+			message.setReject("Not allowed");
+			return;
+		}
+		const BankStatementSubjectPrefix = env.BANK_STATEMENT_SUBJECT_PREFIX;
+		const beginIndex = parsedEmail.subject.indexOf(BankStatementSubjectPrefix) + BankStatementSubjectPrefix.length;
+		const endIndex = parsedEmail.subject.indexOf(",");
+		const bankStatementId = parsedEmail.subject.substring(beginIndex, endIndex).trim();
+		let currentYear = new Date().getUTCFullYear();
+		const attFileName = currentYear + bankStatementId + "-" + currentYear + bankStatementId + ".html";
+		for (const att of parsedEmail.attachments) {
+			const dropBoxResponse = await fetch("https://content.dropboxapi.com/2/files/upload", {
+				method: "POST",
+				headers: {
+				"Authorization": `Bearer ${env.DROPBOX_TOKEN}`,
+				"Content-Type": "application/octet-stream",
+				"Dropbox-API-Arg": '{"path":"/Racunovodstvo KCODE/Izvodi/' + attFileName + '"}'
+				},
+				body: att.content
+			});
+			console.log("DropBox response code: " + dropBoxResponse.status);
+		}
+		console.log(`Forwading message to: ${env.FORWARD_TO}`);
+		await message.forward(env.FORWARD_TO);
 	},
 };
