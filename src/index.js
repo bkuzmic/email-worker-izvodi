01df873ec98b242ed257cec08cbe9c19e46ad017
Boris Kuzmic <boris.kuzmic@bloomreach.com>
2023-12-12 22:16:54 +0100

    Skipping upload if not bank statement as attachment

--- src/index.js
+++ src/index.js
@@ -26,27 +26,30 @@ export default {
 		const parsedEmail = await parser.parse(rawEmail);
 		console.log("Mail subject: ", parsedEmail.subject);
 		if (parsedEmail.attachments.length == 0) {
-			console.log("No attachments - skipping");
-			message.setReject("Not allowed");
-			return;
-		}
-		const BankStatementSubjectPrefix = env.BANK_STATEMENT_SUBJECT_PREFIX;
-		const beginIndex = parsedEmail.subject.indexOf(BankStatementSubjectPrefix) + BankStatementSubjectPrefix.length;
-		const endIndex = parsedEmail.subject.indexOf(",");
-		const bankStatementId = parsedEmail.subject.substring(beginIndex, endIndex).trim();
-		let currentYear = new Date().getUTCFullYear();
-		const attFileName = currentYear + bankStatementId + "-" + currentYear + bankStatementId + ".html";
-		for (const att of parsedEmail.attachments) {
-			const dropBoxResponse = await fetch("https://content.dropboxapi.com/2/files/upload", {
-				method: "POST",
-				headers: {
-				"Authorization": `Bearer ${env.DROPBOX_TOKEN}`,
-				"Content-Type": "application/octet-stream",
-				"Dropbox-API-Arg": '{"path":"/Racunovodstvo KCODE/Izvodi/' + attFileName + '"}'
-				},
-				body: att.content
-			});
-			console.log("DropBox response code: " + dropBoxResponse.status);
+			console.log("No attachments. Skipping sending to DropBox");
+		} else {
+			const BankStatementSubjectPrefix = env.BANK_STATEMENT_SUBJECT_PREFIX;
+			const beginIndex = parsedEmail.subject.indexOf(BankStatementSubjectPrefix) + BankStatementSubjectPrefix.length;
+			if (beginIndex > 0) {
+				const endIndex = parsedEmail.subject.indexOf(",");
+				const bankStatementId = parsedEmail.subject.substring(beginIndex, endIndex).trim();
+				let currentYear = new Date().getUTCFullYear();
+				const attFileName = currentYear + bankStatementId + "-" + currentYear + bankStatementId + ".html";
+				for (const att of parsedEmail.attachments) {
+					const dropBoxResponse = await fetch("https://content.dropboxapi.com/2/files/upload", {
+						method: "POST",
+						headers: {
+						"Authorization": `Bearer ${env.DROPBOX_TOKEN}`,
+						"Content-Type": "application/octet-stream",
+						"Dropbox-API-Arg": '{"path":"/Racunovodstvo KCODE/Izvodi/' + attFileName + '"}'
+						},
+						body: att.content
+					});
+					console.log("DropBox response code: " + dropBoxResponse.status);
+				}
+			} else {
+				console.log("Not a bank statement. Skipping sending to DropBox")
+			}
 		}
 		console.log(`Forwading message to: ${env.FORWARD_TO}`);
 		await message.forward(env.FORWARD_TO);
